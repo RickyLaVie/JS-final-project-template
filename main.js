4dc17073bf00f8cdd4088b43a701e2d1c59919f3
RickyLaVie <rickylai0903@gmail.com>
2016-10-15 17:07:09 +0800

    practice main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var bgImg = document.createElement("img");
-bgImg.src = "images/map.png";
+bgImg.src = "images/maps.png";
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 
