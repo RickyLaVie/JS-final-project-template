522454df6237db42696e0aba0d7ff6a75a4096db
RickyLaVie <rickylai0903@gmail.com>
2016-10-15 17:12:02 +0800

    practice main.js

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 var bgImg = document.createElement("img");
 bgImg.src = "images/map.png";
+var bgImgTower = document.createElement("imgt");
+bgImgTower.src = "images/tower.png";
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 
 function draw(){
    
-   ctx.drawImage(bgImg,0,0);
+   ctx.drawImage(bgImg,bgImgTower,0,0);
 }
 
 draw();
