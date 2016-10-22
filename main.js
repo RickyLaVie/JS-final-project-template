b640a09385ec952a59f51580c0f0ecef85cc2e5d
RickyLaVie <rickylai0903@gmail.com>
2016-10-22 16:48:25 +0800

    Update main.js

--- main.js
+++ main.js
@@ -6,10 +6,6 @@ var enemyImg = document.createElement("img");
 enemyImg.src = "images/slime.gif";
 var buttonImg = document.createElement("img");
 buttonImg.src = "images/tower-btn.png";
-var enemy = {
-   x: 95,
-   y: 430
-};
 var tower = {
    x: 543,
    y: 75
@@ -21,13 +17,30 @@ var btn = {
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 
+$("#game-canvas").mousemove(function(event){
+   console.log("x:" + event.offsetX + ",y:" + event.offsetY);
+});
+
+$("#game-canvas").on("mousemove",function(event){
+   cusor = {
+     x: event.offsetX,
+     y: event.offsetY
+   };
+});
+
 function draw(){
    
    ctx.drawImage(bgImg,0,0);
-   ctx.drawImage(towerImg,tower.x,tower.y);
    ctx.drawImage(enemyImg,enemy.x,enemy.y);
    ctx.drawImage(buttonImg,btn.x,btn.y,65,65);
 }
+
+function clickBtn(){
+   ctx.drawImage(towerImg,cursor.x,cursor.y);
+}
+
+$("#button").on("click",clickBtn);
+
 
 // draw();
 //「setTimeout(draw, 1000);」=「setInterval(draw, 16);」
