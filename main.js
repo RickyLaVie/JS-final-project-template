32038c28821ccb2688c43e7b94ed1cce44fd77cf
RickyLaVie <rickylai0903@gmail.com>
2016-10-29 15:21:05 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
+var canvas = document.getElementById("game-canvas");
+var ctx = canvas.getContext("2d");
+
 var bgImg = document.createElement("img");
 bgImg.src = "images/map.png";
 var towerImg = document.createElement("img");
-towerImg.src = "images/tower.png";
+towerImg.src = "images/Mortar.png";
 var enemyImg = document.createElement("img");
 enemyImg.src = "images/slime.gif";
 var buttonImg = document.createElement("img");
@@ -14,9 +17,6 @@ var btn = {
    x:576,
    y:416
 };
-
-var canvas = document.getElementById("game-canvas");
-var ctx = canvas.getContext("2d");
 
 function draw(){
    
@@ -45,7 +45,15 @@ $("#game-canvas").on("click", function(){
    else if(isBuilding){
       tower.x =cursor.x-cursor.x%32;
       tower.y =cursor.y-cursor.y%32;
+      isBuilding = false;
    }
+});
+
+$("#game-canvas").on("mousemove",function(event){
+   cursor = {
+     x: event.offsetX,
+     y: event.offsetY
+   };
 });
 
 
@@ -61,12 +69,7 @@ function isCollided(pointX, pointY, targetX, targetY, targetWidth, targetHeight)
     }
 }
 
-$("#game-canvas").on("mousemove",function(event){
-   cursor = {
-     x: event.offsetX,
-     y: event.offsetY
-   };
-});
+setInterval(draw, 16);
 
 // function clickBtn(){
 //    ctx.drawImage(towerImg,cursor.x,cursor.y);
@@ -77,5 +80,5 @@ $("#game-canvas").on("mousemove",function(event){
 
 // draw();
 //「setTimeout(draw, 1000);」=「setInterval(draw, 16);」
-setInterval(draw, 16);
+// var FPS = 60;
 // setInterval(draw, 1000/FPS);
