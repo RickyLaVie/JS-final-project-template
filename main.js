c72d1a2d29da221d32bf94973cbc6b9e9a51396d
RickyLaVie <rickylai0903@gmail.com>
2016-11-05 17:49:05 +0800

    Update main.js

--- main.js
+++ main.js
@@ -54,9 +54,12 @@ var enemy = {
          this.speedx=0;
          this.speedy=-64;
       }
+      else {
+         this.x = this.x + this.speedx/FPS;
+         this.y = this.y + this.speedy/FPS;
+      }
    }
-   this.x = this.x + this.speedx/FPS;
-   this.y = this.y + this.speedy/FPS;
+   
 };
 
 var btn = {
